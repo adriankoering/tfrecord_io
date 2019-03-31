5b08187c2df2138613775c6f3f6bd32a1bbc6947
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2019-03-31 22:19:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 setup_args = generate_distutils_setup(
     name="TFRecord_IO",
     version="0.1",
+    description='The funniest joke in the world',
+    url='https://github.com/adriankoering/tfrecord_io',
+    author='Adrian Köring',
+    license='MIT',
     packages=find_packages(),
     install_requires=["tensorflow", "PIL", "pathlib"]
 )
