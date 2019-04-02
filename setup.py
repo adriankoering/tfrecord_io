b5a923ee1442705584f0ce3e4b4f9526508d91e9
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2019-04-02 14:04:02 +0200

    Remove tensorflow from dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name="TFRecord_IO",
     version="0.1",
-    description='The funniest joke in the world',
+    description='Utilities to read and write tfrecord files',
     url='https://github.com/adriankoering/tfrecord_io',
     author='Adrian Köring',
     license='MIT',
     packages=find_packages(),
-    install_requires=["tensorflow", "pillow", "pathlib"]
+    install_requires=["pillow", "pathlib"]
 )
