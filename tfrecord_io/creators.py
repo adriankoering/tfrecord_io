2427389ec5b06c8cff63bff68862e76e342bd649
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2019-11-30 01:13:06 +0100

    Completely restructure how tf-record creators work internally
        (dry-er now)

--- tfrecord_io/creators.py
+++ tfrecord_io/creators.py
@@ -6,128 +6,216 @@ from typing import Any, Dict, List, Tuple
 import numpy as np
 import tensorflow as tf
 from tfrecord_io.features import bytes_feature, float_feature, int64_feature
+from tfrecord_io.image import encode_image
 
 
-def create_detection_example(
-        jpeg_encoded_image: bytes, image_shape: Tuple[int, int, int],
-        boxes: np.ndarray, classes: List[int], classes_text: List[bytes],
-        filename: str = "") -> tf.train.Example:
-  """
-  Create an example compatible with tensorflow's object detection api.
+def add_image_features(image: np.ndarray, format="jpeg"):
+  H, W, C = image.shape
+  encoded_image = encode_image(image, format=format)
+  return {
+      "image/encoded": bytes_feature(encoded_image),
+      "image/format": bytes_feature(format.encode('utf8')),
+      "image/height": int64_feature(H),
+      "image/width": int64_feature(W),
+      "image/channels": int64_feature(C),
+  }
 
-  Args:
-    jpeg_encoded_image:
-      jpeg encoded / compressed image
-    image_shape:
-      [height, width, channels] dimension of the image
-    boxes:
-      bounding boxes with shape [NumBoxes, 4] in relative coordinates and in
-      [left, top, right, bottom] order
-    classes:
-      class id's according to labelmap
-    classes_text:
-      binary encoded text representation of the class
-    filename:
-      name of the image file on disk
-  """
-  h, w, c = image_shape
-  boxes = np.array(boxes)
+
+def add_classification_features(labelid: int):
+  return {"image/class/label": int64_feature(labelid)}
+
+
+def add_probability_features(probabilities):
+  if isinstance(probabilities, np.ndarray):
+    probabilities = probabilities.flatten().tolist()
+    return {"image/class/prob": float_feature(probabilities)}
+
+
+def add_detection_features(boxes: np.ndarray, labelids: List[int],
+                           class_names: List[bytes]):
+  if not isinstance(boxes, np.ndarray):
+    boxes = np.array(boxes)
   if len(boxes):
     left, top, right, bottom = boxes.T
   else:
     left, top, right, bottom = [], [], [], []
-  example = tf.train.Example(
-      features=tf.train.Features(
-          feature={
-              'image/height': int64_feature(h),
-              'image/width': int64_feature(w),
-              'image/channels': int64_feature(c),
-              'image/filename': bytes_feature(filename.encode("utf8")),
-              'image/encoded': bytes_feature(jpeg_encoded_image),
-              'image/format': bytes_feature('jpeg'.encode('utf8')),
-              'image/object/bbox/xmin': float_feature(left),
-              'image/object/bbox/ymin': float_feature(top),
-              'image/object/bbox/xmax': float_feature(right),
-              'image/object/bbox/ymax': float_feature(bottom),
-              'image/object/class/text': bytes_feature(classes_text),
-              'image/object/class/label': int64_feature(classes)
-          }))
-  return example
+
+  return {
+      "image/object/bbox/xmin": float_feature(left),
+      "image/object/bbox/ymin": float_feature(top),
+      "image/object/bbox/xmax": float_feature(right),
+      "image/object/bbox/ymax": float_feature(bottom),
+      "image/object/class/label": int64_feature(labelids),
+      "image/object/class/text": bytes_feature(class_names)
+  }
 
 
-def create_classification_example(
-        encoded_image: bytes, image_shape: Tuple[int, int, int],
-        image_format: str, class_label: int,
-        probabilities: List[float]) -> tf.train.Example:
+def add_instance_segmentation_features(instance_masks: List[np.ndarray]):
+  png_encoded_masks = [encode_image(mask) for mask in instance_masks]
+  return {"image/object/mask": bytes_feature(png_encoded_masks)}
+
+
+def add_segmentation_features(segmentation_mask: np.ndarray):
+  png_encoded_mask = encode_image(segmentation_mask)
+  return {
+      "image/segmentation/class/encoded": bytes_feature(png_encoded_mask),
+      "image/segmentation/class/format": bytes_feature("png".encode("utf8"))
+  }
+
+
+def create_image_example(image: np.ndarray,
+                         encoding_format: str = "jpeg") -> tf.train.Example:
+  """ Create a tensorflow example containing a bare (unannotated) image
+
+  Args:
+    image: image to write into the example
+    encoding_format: compression format to apply to the image (eg jpeg or png)
+
+  Returns:
+    example: tf.train.Example
+  """
+  image_features = add_image_features(image, format=encoding_format)
+  return tf.train.Example(features=tf.train.Features(feature=image_features))
+
+
+def create_classification_example(image: np.ndarray,
+                                  labelids: int,
+                                  encoding_format: str = "jpeg"
+                                 ) -> tf.train.Example:
   """ Create a tensorflow 'classification' Example with image and class label
 
   Args:
-    encoded_image: encoded image file (aka compressed image (eg jpg or png))
-    image_shape: [height, width, num_color_channels] of the image
-    image_format: compression format of the image (eg jpeg or png)
-    class_label: the class index to which the image blongs
-    probabilities: probability distribution (usually one_hot encoded class)
+    image: image to write into the examle
+    labelids: the class index to which the image belongs
+    encoding_format: compression format of the image (eg jpeg or png)
 
   Returns:
-    Example containing all information, ready to be serialized into a tfrecord
+    example: tf.train.Example
   """
-  h, w, c = image_shape
-  example = tf.train.Example(
-      features=tf.train.Features(
-          feature={
-              "image/height": int64_feature(h),
-              "image/width": int64_feature(w),
-              "image/channels": int64_feature(c),
-              "image/encoded": bytes_feature(encoded_image),
-              "image/format": bytes_feature(image_format.encode("utf8")),
-              "image/class/label": int64_feature(class_label),
-              "image/class/prob": float_feature(probabilities),
-          }))
-  return example
+  image_features = add_image_features(image, format=encoding_format)
+  label_features = add_classification_features(labelids)
+  features = {**image_features, **label_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
 
 
-def create_image_example(encoded_image: bytes,
-                         image_shape: Tuple[int, int, int],
-                         image_format: str) -> tf.train.Example:
-  """ Create a tensorflow 'image' Example with image only
-
-  Args:
-    encoded_image: encoded image file (aka compressed image (eg jpg or png))
-    image_shape: [height, width, num_color_channels] of the image
-    image_format: compression format of the image (eg jpeg or png)
-
-  Returns:
-    Example containing all information, ready to be serialized into a tfrecord
-  """
-  h, w, c = image_shape
-  example = tf.train.Example(
-      features=tf.train.Features(
-          feature={
-              "image/height": int64_feature(h),
-              "image/width": int64_feature(w),
-              "image/channels": int64_feature(c),
-              "image/encoded": bytes_feature(encoded_image),
-              "image/format": bytes_feature(image_format.encode("utf8")),
-          }))
-  return example
-
-
-def create_probability_example(classes: int,
-                               probabilities: List[float]) -> tf.train.Example:
+def create_probability_example(image: np.ndarray,
+                               labelids: int,
+                               probabilities: List[float],
+                               encoding_format: str = "jpeg"
+                              ) -> tf.train.Example:
   """ Create a probability Example with only probability targets
 
   Args:
-   classes: class index
+   image: image to write into the examle
+   labelids: the class index to which the image belongs
    probabilities: list of np.ndarray
      NumClasses long list with per-class probabilities in each entry
+   encoding_format: compression format of the image (eg jpeg or png)
 
   Returns:
-     Example containing all information, ready to be serialized into a tfrecord
+    example: tf.train.Example
   """
-  if isinstance(probabilities, np.ndarray):
-    probabilities = probabilities.flatten().tolist()
-  example = tf.train.Example(
-      features=tf.train.Features(
-          feature={"image/class/prob": float_feature(probabilities),
-                   "image/class/label": int64_feature(classes)}))
-  return example
+  image_features = add_image_features(image, format=encoding_format)
+  label_features = add_classification_features(labelids)
+  prob_features = add_probability_features(probabilities)
+  features = {**image_features, **label_features, **prob_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
+
+
+def create_detection_example(image: np.ndarray,
+                             boxes: np.ndarray,
+                             labelids: List[int],
+                             class_names: List[bytes],
+                             encoding_format: str = "jpeg") -> tf.train.Example:
+  """
+  Create an example compatible with tensorflow's object detection api.
+
+  Args:
+    image: image to write into the example
+    boxes: bounding boxes with shape [NumBoxes, 4] in relative coordinates
+            and in [left, top, right, bottom] order
+    classes:
+      class id's according to labelmap
+    classes_text:
+      binary encoded text representation of the class
+
+  Returns:
+    example: tf.train.Example
+  """
+  image_features = add_image_features(image, format=encoding_format)
+  detection_features = add_detection_features(boxes, labelids, class_names)
+  features = {**image_features, **detection_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
+
+
+def create_instance_segmentation_example(image: np.ndarray,
+                                         boxes: np.ndarray,
+                                         labelids: List[int],
+                                         class_names: List[bytes],
+                                         masks: List[np.ndarray],
+                                         encoding_format: str = "jpeg"
+                                        ) -> tf.train.Example:
+  """
+  Create an example compatible with tensorflow's object detection api.
+
+  Args:
+    image: image to write into the example
+    boxes: bounding boxes with shape [NumBoxes, 4] in relative coordinates
+            and in [left, top, right, bottom] order
+    classes:
+      class id's according to labelmap
+    classes_text:
+      binary encoded text representation of the class
+
+  Returns:
+    example: tf.train.Example
+  """
+  image_features = add_image_features(image, format=encoding_format)
+  detection_features = add_detection_features(boxes, labelids, class_names)
+  mask_features = add_instance_segmentation_features(masks)
+  features = {**image_features, **detection_features, **mask_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
+
+
+def create_segmentation_example(image: np.ndarray,
+                                segmentation: np.ndarray,
+                                encoding_format: str = "jpeg"
+                               ) -> tf.train.Example:
+  """
+  Create an example compatible with tensorflow's object detection api.
+
+  Args:
+    image: image to write into the example
+    segmentation: of the image assigning each pixel a labelid
+
+  Returns:
+    example: tf.train.Example
+  """
+  image_features = add_image_features(image, format=encoding_format)
+  segmentation_features = add_segmentation_features(segmentation)
+  features = {**image_features, **segmentation_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
+
+
+def create_classification_and_segmentation_example(image: np.ndarray,
+                                                   labelid: int,
+                                                   segmentation: np.ndarray,
+                                                   encoding_format: str = "jpeg"
+                                                  ) -> tf.train.Example:
+  """
+  Create an example compatible with tensorflow's object detection api.
+
+  Args:
+    image: image to write into the example
+    labelid: the class index to which the image belongs
+    segmentation: of the image assigning each pixel a labelid
+    encoding_format: compression format of the image (eg jpeg or png)
+
+  Returns:
+    example: tf.train.Example
+  """
+  image_features = add_image_features(image, format=encoding_format)
+  label_features = add_classification_features(labelid)
+  segmentation_features = add_segmentation_features(segmentation)
+  features = {**image_features, **label_features, **segmentation_features}
+  return tf.train.Example(features=tf.train.Features(feature=features))
